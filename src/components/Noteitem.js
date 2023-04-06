720b304f94fe206c47293c97f112806bf5fefd82
Pradosh <chandpradosh7@gmail.com>
2023-04-06 14:16:55 +0530

    Fixed Navbar and updated Noteitem

--- src/components/Noteitem.js
+++ src/components/Noteitem.js
@@ -11,12 +11,12 @@ const Noteitem = (props) => {
             <div className="cardContainer">
                 <div className="cardtop">
                     <i className="fa-solid fa-trash" onClick={async ()=>{await deleteNote(note._id);  props.showAlert("Successful","Note deleted!","greenAlert")}}></i>
-                    <span className="tag tagtext">{note.tag}</span>
                     <i className="fa-solid fa-pen-to-square" onClick={()=>{updateNote(note)}}></i>
                 </div>
                 <div className="cardbottom">
                     <h4 className='titletext'><b>{props.Capitalize(note.title)}</b></h4>
                     <p className='paratext'>{note.description}</p>
+                    <span className="tag tagtext">{note.tag}</span>
                 </div>
             </div>
         </>
